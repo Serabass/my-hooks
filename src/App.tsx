5d40e492226cc998990596729a43a16072ed4c45
Serabass <serabass565@gmail.com>
2021-12-12 01:14:57 +0600

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Col, Input, Row } from 'antd';
+import { Col, InputNumber, Row } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
-import { createUseLocalStorage } from './shared/hooks/useLocalStorage';
-
-let useLocalStorage = createUseLocalStorage('app');
+import { useAjax } from './shared/hooks/useAjax';
 
 function App() {
-  let [value, setValue] = useLocalStorage('value', 'myValue');
+  let a = useAjax({
+    url: 'https://httpbin.org/get',
+  });
 
   return (
     <div className="App">
       <Row>
-        <Col md={4}>
-          <Input
-            value={value}
-            onChange={(e) => {
-              setValue(e.target.value);
-            }}
-          />
-        </Col>
+        <Col md={4}></Col>
       </Row>
     </div>
   );
--- src/shared/hooks/useAjax.ts
+++ src/shared/hooks/useAjax.ts
@@ -1,16 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TTL } from '../chronoparse';
-import 'node-fetch';
-import { LocalForageCacheDriver } from '../ajax/remember';
 import axios, { Method } from 'axios';
-import { DefaultCacheKeyBuilder } from '../ajax/CacheKeyBuilder';
-
-export interface CacheOptions {
-  enabled?: boolean;
-  ttl: TTL;
-}
-
-export type CacheField = boolean | TTL | CacheOptions;
 
 export interface UseAjaxProps<TRes> {
   url: string;
@@ -19,80 +8,29 @@ export interface UseAjaxProps<TRes> {
   data?: any;
   headers?: any;
   loading?: React.ReactElement | null;
-  cache?: CacheField;
 }
 
 export interface UseAjaxResult<T> {
   response: { data: T };
   loading: boolean;
-  cached: CacheField;
   error: any;
 }
 
-export function cacheEnabled(value?: CacheField): boolean {
-  if (typeof value === 'undefined') {
-    return false;
-  }
-
-  if (typeof value === 'boolean') {
-    return value;
-  }
-
-  if (typeof value === 'object') {
-    if (typeof value.enabled === 'undefined') {
-      return true;
-    }
-    return value.enabled;
-  }
-
-  if (typeof value === 'string' || typeof value === 'number') {
-    return true;
-  }
-
-  return false;
-}
-
-export function useAjax<T>({ url, method = 'GET', params, data, headers, cache }: UseAjaxProps<T>): [UseAjaxResult<T>] {
+export function useAjax<T>({ url, method = 'GET', params, data, headers }: UseAjaxProps<T>): [UseAjaxResult<T>] {
   let [response, setResponse] = useState<{ data: T }>();
   let [loading, setLoading] = useState(false);
-  let [cached, setCached] = useState(false);
   let [error, setError] = useState<Error>();
 
   useEffect(() => {
-    let key = DefaultCacheKeyBuilder.instance({ url, method, params, data, headers }).build();
-    let cacheTtl: TTL = 0;
-
-    if (typeof cache === 'object') {
-      cacheTtl = cache.ttl;
-    } else if (typeof cache === 'boolean') {
-      cacheTtl = '10sec';
-    } else if (typeof cache === 'string' || typeof cache === 'number') {
-      cacheTtl = cache;
-    }
-
-    LocalForageCacheDriver.instance
-      .remember<{ data: T }>(
-        key,
-        cacheTtl,
-        cacheEnabled(cache),
-        () => {
-          setLoading(true);
-          return axios
-            .request<T>({
-              method,
-              url,
-              params,
-              headers,
-              data,
-            })
-            .then((res) => ({
-              data: res.data,
-            }));
-        },
-        (cached: any) => {
-          setCached(cached);
-        },
-      )
+    setLoading(true);
+    axios
+      .request<T>({
+        method,
+        url,
+        params,
+        headers,
+        data,
+      })
       .then((res: any) => {
         setResponse(res);
         setLoading(false);
@@ -101,13 +39,12 @@ export function useAjax<T>({ url, method = 'GET', params, data, headers, cache }
         setError(res);
         setLoading(false);
       });
-  }, [data, headers, url, params, method, cache]);
+  }, [data, headers, url, params, method]);
 
   return [
     {
       response: response as any,
       loading,
-      cached,
       error,
     },
     // refetch
